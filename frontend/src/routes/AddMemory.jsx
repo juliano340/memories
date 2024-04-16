46b3921d7d51d18a9f86f3b17c2e5fefddd2910d
Juliano Pereira <juliano340@gmail.com>
2024-04-16 01:36:14 -0300

    Adicionado formulario de memória

--- frontend/src/routes/AddMemory.jsx
+++ frontend/src/routes/AddMemory.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
+import './AddMemory.css'
 
 export default function AddMemory() {
   return (
-    <div>AddMemory</div>
+    <div className="add-memory-page">
+      <h2>Adicionar Memória</h2>
+      <form>
+        <label>
+          <p>Título</p>
+          <input type="text" name="title" id="" placeholder='Insira um título'/>
+        </label>
+        <label>
+          <p>Descrição</p>
+          <textarea name="description"  placeholder='Descreva sua memória'></textarea>
+        </label>
+        <label>
+          <p>Foto</p>
+          <input type="file" name="image" id="" placeholder='Insira um título'/>
+        </label>
+        <input className='btn' type="submit" value="Enviar" />
+      </form>
+    </div>
   )
 }
