3a7432a29a49098efb372ca9eb9f37ba3dab6df8
Juliano Pereira <juliano340@gmail.com>
2024-04-16 03:14:01 -0300

    Ajustes na HOme

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,7 +1,50 @@
 import React from 'react'
+import axios from '../axios-config'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import './Home.css'
 
-export default function Home() {
+
+const Home = () => {
+
+  const [memories, setMemories] = useState([])
+  
+
+  useEffect(() => {
+    const getMemories = async () => {
+      try {
+        const response = await axios.get('/memories')
+        setMemories(response.data.memories);  // Correção aqui
+        console.log(response.data.memories);  // Log corrigido para mostrar os dados corretos
+      } catch (error) {
+        console.error("Erro ao buscar memórias:", error);
+      }
+      
+    }
+
+    getMemories()
+    
+  }, [])
+
   return (
-    <div>Home</div>
+    
+    <div className='home'>
+      <h2>Últimas memórias</h2>
+      <div className="memories-container">
+      {memories.length === 0 && <h2>Nenhuma memória encontrada :(</h2>}
+        {memories.length > 0 && memories.map((memory) =>
+        
+          <div className='memory' key={memory._id}>
+            <img src={`${axios.defaults.baseURL}${memory.src}`} alt={memory.title} />
+            <p>{memory.title}</p>
+            <Link className='btn' to={`/memory/${memory._id}`}>Comentar</Link>
+          </div>   
+      
+      )}
+      </div>
+    </div>
+
   )
 }
+
+export default Home;
